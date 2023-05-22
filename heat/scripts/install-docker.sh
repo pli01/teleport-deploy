b0eb7e5de450ad30f7439ae2679da80b96a501dc
pli01 <pli01@users.noreply.github.com>
2023-05-22 11:13:04 +0200

    fix install-docker

--- heat/scripts/install-docker.sh
+++ heat/scripts/install-docker.sh
@@ -2,14 +2,16 @@
 set -euo pipefail
 # docker
 http_proxy="${http_proxy:-}"
+https_proxy="${http_proxy:-}"
 no_proxy="${no_proxy:-}"
-IP=$(hostname -I)
+IP=$(hostname -I | cut -d' ' -f1)
+
 
 apt-get -qy update
 apt-get -qy install ca-certificates curl gnupg
 install -m 0755 -d /etc/apt/keyrings
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --batch --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod 644 /etc/apt/keyrings/docker.gpg
 
 echo   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
